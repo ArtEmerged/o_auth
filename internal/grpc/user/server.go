6fa1572a28c74b9af9464a18a1c24ed90c2f29f3
artyom_mac <titan4590it@gmail.com>
2024-07-28 18:31:49 +0300

    updated server_grpc layer

--- internal/grpc/user/server.go
+++ internal/grpc/user/server.go
@@ -3,13 +3,17 @@ package grpc_user
 import (
 	"google.golang.org/grpc"
 
+	def "github.com/ArtEmerged/o_auth-server/internal/definitions"
 	desc "github.com/ArtEmerged/o_auth-server/pkg/auth_v1"
 )
 
+var _ desc.UserV1Server = (*userServer)(nil)
+
 type userServer struct {
 	desc.UnimplementedUserV1Server
+	service def.UserService
 }
 
-func Register(s *grpc.Server) {
-	desc.RegisterUserV1Server(s, &userServer{})
+func Register(s *grpc.Server, service def.UserService) {
+	desc.RegisterUserV1Server(s, &userServer{service: service})
 }
--- internal/grpc/user/user.go
+++ internal/grpc/user/user.go
@@ -2,26 +2,31 @@ package grpc_user
 
 import (
 	"context"
+	"errors"
 
-	"github.com/brianvoe/gofakeit"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"google.golang.org/protobuf/types/known/timestamppb"
 
+	def "github.com/ArtEmerged/o_auth-server/internal/definitions"
 	desc "github.com/ArtEmerged/o_auth-server/pkg/auth_v1"
 )
-
-var _ desc.UserV1Server = (*userServer)(nil)
 
 func (s *userServer) CreateUser(ctx context.Context, in *desc.CreateUserRequest) (*desc.CreateUserResponse, error) {
 	if in.GetName() == "" || in.GetEmail() == "" || in.GetPassword() == "" || in.GetPasswordConfirm() == "" {
 		return nil, status.Error(codes.InvalidArgument, "all fields're required")
 	}
 
-	id := gofakeit.Number(1, 99999)
-	
-	return &desc.CreateUserResponse{Id: int64(id)}, nil
+	if in.GetPassword() != in.GetPasswordConfirm() {
+		return nil, status.Error(codes.InvalidArgument, "passwords do not match")
+	}
+
+	id, err := s.service.CreateUser(ctx, def.AdaptedCreateUserRequestToLocal(in))
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &desc.CreateUserResponse{Id: id}, nil
 }
 
 func (s *userServer) GetUser(ctx context.Context, in *desc.GetUserRequest) (*desc.GetUserResponse, error) {
@@ -29,20 +34,24 @@ func (s *userServer) GetUser(ctx context.Context, in *desc.GetUserRequest) (*des
 		return nil, status.Error(codes.InvalidArgument, "negative id")
 	}
 
-	userInfo := &desc.UserInfo{
-		Id:        in.GetId(),
-		Name:      gofakeit.Name(),
-		Email:     gofakeit.Email(),
-		Timestamp: &desc.UserInfo_CreatedAt{timestamppb.New(gofakeit.Date())},
-		Role:      desc.Role_USER,
+	user, err := s.service.GetUser(ctx, in.GetId())
+	if err != nil {
+		if errors.As(err, &def.ErrNotFound) {
+			return nil, status.Error(codes.NotFound, err.Error())
+		}
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
-	return &desc.GetUserResponse{UserInfo: userInfo}, nil
+	return &desc.GetUserResponse{UserInfo: user.ToProto()}, nil
 }
-
 func (s *userServer) UpdateUser(ctx context.Context, in *desc.UpdateUserRequest) (*emptypb.Empty, error) {
 	if in.GetId() < 1 {
 		return nil, status.Error(codes.InvalidArgument, "negative id")
+	}
+
+	err := s.service.UpdateUser(ctx, def.AdaptedUpdateUserRequestToLocal(in))
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	return nil, nil
@@ -51,6 +60,11 @@ func (s *userServer) UpdateUser(ctx context.Context, in *desc.UpdateUserRequest)
 func (s *userServer) DeleteUser(ctx context.Context, in *desc.DeleteUserRequest) (*emptypb.Empty, error) {
 	if in.GetId() < 1 {
 		return nil, status.Error(codes.InvalidArgument, "negative id")
+	}
+
+	err := s.service.DeleteUser(ctx, in.GetId())
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
 	return nil, nil
