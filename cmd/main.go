2750635947bad1b175a181feee7e553006d357d0
artyom_mac <titan4590it@gmail.com>
2024-07-28 18:32:42 +0300

    updated main and added config

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"net"
+	"os/signal"
+	"syscall"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
+	"github.com/ArtEmerged/o_auth-server/config"
 	grpc_user "github.com/ArtEmerged/o_auth-server/internal/grpc/user"
+	"github.com/ArtEmerged/o_auth-server/internal/repository"
+	service "github.com/ArtEmerged/o_auth-server/internal/sevice"
+	"github.com/ArtEmerged/o_auth-server/pkg/database"
 )
 
 func main() {
-	l, err := net.Listen("tcp", ":50051")
+	cfg := config.New()
+	cfg.Init("")
+
+	db := database.NewPostgres(cfg.GetDbDNS())
+	repo := repository.New(db)
+	service := service.New(repo, cfg.SaltPassword)
+
+	l, err := net.Listen("tcp", cfg.GetServerAddress())
 	if err != nil {
 		panic(err)
 	}
@@ -20,11 +33,22 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	grpc_user.Register(s)
+	grpc_user.Register(s, service)
 
-	fmt.Println("listen on :50051")
+	go func() {
+		log.Printf("listen on :%s", cfg.ServerPort)
+		if err = s.Serve(l); err != nil {
+			log.Println(err)
+		}
+	}()
 
-	if err = s.Serve(l); err != nil {
-		log.Println(err)
-	}
+	quit, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	<-quit.Done()
+
+	s.GracefulStop()
+	log.Println("GRPC server has been shut down")
+	db.Close()
+	log.Println("Database closed")
 }
